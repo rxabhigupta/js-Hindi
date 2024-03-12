ba0a41e95c85286b3d4f8d100c69fcc0778085d4
Abhishek Gupta <133134516+rxabhigupta@users.noreply.github.com>
2024-03-12 09:11:30 +0000

    objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,12 @@
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh",
+// }
+
+// //course.courseInstructor
+
+const{courseInstructor: Instructor} = course
+console.log(courseInstructor);
+console.log(Instructor);
+
