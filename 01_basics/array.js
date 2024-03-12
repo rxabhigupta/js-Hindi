4f42bfb313f21f3a4955dc0e2641b1882a8d7be6
Abhishek Gupta <133134516+rxabhigupta@users.noreply.github.com>
2024-03-12 07:45:40 +0000

    array1

---
+++ 01_basics/array.js
@@ -0,0 +1,17 @@
+// const myArr = [1,2,3,4,5]
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+
+// myArr.push(2)
+// myArr.push(6)
+// myArr.push(9)
+// console.log(myArr);
+// console.log(myArr2);
+
+// myArr.unshift(19);
+// console.log(myArr);
+// myArr.shift(12);
+// console.log(myArr);
+const myArr = [1,2,3,4,5]
+myArr.shift(9)
+console.log(myArr);
