6a1a2b028afae108fa21a4382b7cfddb6bbc3443
Abhishek Gupta <133134516+rxabhigupta@users.noreply.github.com>
2024-03-13 07:28:47 +0000

    functions and high loop

---
+++ 04_basics/high_loop.js
@@ -0,0 +1,28 @@
+//other Basic loops we already know....Now these are higher order loops...
+//for of
+
+// const arr = [1 ,2 ,3 ,4 ,5]
+
+// for(const num of object){//kis object pe loop lagana hai ...here object doesn't refering object used in programming
+    
+// }
+// for(const num of arr){
+//     console.log(num);
+// }
+
+// const replying = "Hello World!"
+// for(const reply of replying){
+//     console.log(`Each char is a ${reply}`)
+// }
+
+// //maps
+// const map = new Map();
+
+// map.set('IN', "India");
+// map.set('CND', "Canada");
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
